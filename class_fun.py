941cee0e290a831fbfd643e124fe56a347dafffa
KFeyn <kir-fein@yandex.ru>
2021-06-29 21:52:48 +0300

    Добавил тестовый коммент из пайчарма

--- class_fun.py
+++ class_fun.py
@@ -10,6 +10,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
+# тест
 
 class StageResults:
     """
